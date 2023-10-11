cf542fb7c19e7f70d9d592a8b4d0b5c886607a1b
juan <juan@juan>
2023-10-10 23:30:28 -0300

    remaking the main function

--- functions/caesar-cipher.js
+++ functions/caesar-cipher.js
@@ -3,6 +3,9 @@ function cipher (msg, factor){
     const abc2  = ["a","b","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
     const abc  = " abcdefghijklmnñopqrstuvwxyz";
 
+    const numbers = "0123456789";
+    const symbols = "-.,+{}¿?|_;:][¨*";
+
     let cipheredMsg = "";
 
     let abcIdx;
@@ -14,20 +17,52 @@ function cipher (msg, factor){
 
             if(char == abc[i]){
 
-                if(i>=27)
+                if(i >= 26 || i >= 27 || i >= 28 )
+                    
+                    cipheredMsg += abc[i-3];
+                else
+                    cipheredMsg += abc[i+3];
+
+            } else if (char == numbers[i]){
+
+            } else if (char == symbols[i]){
+
+            }
+            
+
+            
+        }
+    }
+    
+    console.log(cipheredMsg);
+
+}
+
+cipher("x",3);
+
+
+/*
+
+if(char == abc[i]){
+
+                if(i>=26 || i>=27 || i>=28)
                     cipheredMsg += abc[i-3];
 
                 else if(char == " ")
                     cipheredMsg += " ";
                 
-                else if(i<=28)
+                    //here a regex could be helpful to detect a symbol and then assign it to the variable.
+                else if(char == "<" ||
+                        char == "+" ||
+                        char == "-" ||
+                        char == "." )
+                    cipheredMsg += "+"
+                    //here I discover I should re-build the abc array, adding numbrers and symbolds. This implies the re-thinkeering of this horrorific loop
+                else if(char == 1)
+                    cipheredMsg += 1;    
+                
+                else if(i<=26)
                     cipheredMsg += abc[i+3];
             }
-        }
-    }        
 
-    console.log(cipheredMsg);
-
-}
-
-cipher("buenas ",3);
+*/
---
+++ tests/caesar-cipher.test.js
@@ -0,0 +1,14 @@
+const cipher = require('../functions/caesar-cipher.js');
+
+
+test('Testing letters', () =>{
+
+
+    expect(cipher("pedo",3)).toBe("shkr");
+    expect(cipher("zoy nacho",3)).toBe("wr");
+
+
+});
+
+
+test("Testing number",)
