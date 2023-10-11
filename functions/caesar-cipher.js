b666e820df1f395a64180d67ef34b504fd78ae3b
SaloJuan <78494851+SaloJuan@users.noreply.github.com>
2023-10-11 09:19:20 -0300

    Update caesar-cipher.js. Now it's prepared to test numbers, symbols and return the same case for letters. In the future it'd be practical to separate functionalities in outter functions.

--- functions/caesar-cipher.js
+++ functions/caesar-cipher.js
@@ -2,6 +2,7 @@ function cipher (msg, factor){
 
     const abc2  = ["a","b","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
     const abc  = " abcdefghijklmnñopqrstuvwxyz";
+    const abcUpp = " ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
     const numbers = "0123456789";
     const symbols = "-.,+{}¿?|_;:][¨*";
@@ -15,22 +16,31 @@ function cipher (msg, factor){
 
         for (let i=0; i<abc.length; i++){
 
-            if(char == abc[i]){
+            if(char == abc[i] || char == abcUpp[i])
+            {
 
-                if(i >= 26 || i >= 27 || i >= 28 )
-                    
-                    cipheredMsg += abc[i-3];
-                else
-                    cipheredMsg += abc[i+3];
+                if(i >= 26 || i >= 27 || i >= 28 ){
+
+                    if(char == abcUpp[i])
+                        cipheredMsg += abcUpp[i-3];
+                    else
+                        cipheredMsg +=abc[i-3];
+
+                } else {
+
+                    console.log('else letra comun a validar si es mayuscula')
+
+                    if(char == abcUpp[i])
+                        cipheredMsg += abcUpp[i+3];
+                    else
+                        cipheredMsg += abc[i+3];
+                }                    
 
             } else if (char == numbers[i]){
 
             } else if (char == symbols[i]){
 
-            }
-            
-
-            
+            }            
         }
     }
     
@@ -38,31 +48,4 @@ function cipher (msg, factor){
 
 }
 
-cipher("x",3);
-
-
-/*
-
-if(char == abc[i]){
-
-                if(i>=26 || i>=27 || i>=28)
-                    cipheredMsg += abc[i-3];
-
-                else if(char == " ")
-                    cipheredMsg += " ";
-                
-                    //here a regex could be helpful to detect a symbol and then assign it to the variable.
-                else if(char == "<" ||
-                        char == "+" ||
-                        char == "-" ||
-                        char == "." )
-                    cipheredMsg += "+"
-                    //here I discover I should re-build the abc array, adding numbrers and symbolds. This implies the re-thinkeering of this horrorific loop
-                else if(char == 1)
-                    cipheredMsg += 1;    
-                
-                else if(i<=26)
-                    cipheredMsg += abc[i+3];
-            }
-
-*/
+cipher("A",3);
