e67e6f1041d6801966a8922ee491a286dbc4ce76
juan <juan@juan>
2023-10-13 00:59:09 -0300

    finished function's logic, beginning with the test fn

---
+++ functions/analyze-array.js
@@ -0,0 +1,49 @@
+function analyzeArray(arr){
+
+    let isNumberArr = true;
+
+    arr.forEach(elem => {
+        
+        if(typeof elem != "number") 
+            isNumberArr = false;
+    })
+
+    if (isNumberArr){
+
+        let arrObj = {
+
+            min: 0,
+            max: 0,
+            average: 0,
+            length: arr.length
+    
+        }            
+    
+        arr.sort((a,b) =>{
+    
+            return a-b;
+        })
+    
+        arrObj.min = arr[0];
+    
+        arrObj.max = arr[arr.length-1];
+    
+        let total = 0;
+    
+        arr.forEach(elem => {            
+            
+            total = total + elem;
+            
+        });    
+    
+        arrObj.average = total/arr.length;
+    
+        return arrObj;    
+
+    } else
+
+        throw new Error("enter a valid numbers array");    
+
+}
+
+module.exports = analyzeArray;
